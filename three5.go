9c27e07caa4fdb23587c11b34e0bcca7e5c39acc
a <a@fuhq>
2020-06-12 06:06:08 -0400

    more splice commands

--- three5.go
+++ three5.go
@@ -15,6 +15,7 @@ func DeB64(b64 string) []byte {
 
 // SpInfo is the splice info section of the SCTE 35 cue.
 type SpInfo struct {
+	Name                   string
 	TableId                string
 	SectionSyntaxIndicator bool
 	Private                bool
@@ -32,6 +33,7 @@ type SpInfo struct {
 
 // Decode extracts bits for the splice info section values
 func (spi *SpInfo) Decode(bitn *gobit.Bitn) {
+	spi.Name = "Splice Info Section"
 	spi.TableId = bitn.AsHex(8)
 	spi.SectionSyntaxIndicator = bitn.AsBool()
 	spi.Private = bitn.AsBool()
@@ -50,21 +52,22 @@ func (spi *SpInfo) Decode(bitn *gobit.Bitn) {
 // SpCmd is the splice command for the SCTE35 cue
 type SpCmd struct {
 	Name                       string
-	BreakAutoReturn            bool
-	BreakDuration              float64
-	TimeSpecifiedFlag          bool
-	PTS                        float64
-	SpliceEventId              uint64
+	SpliceEventId              string
 	SpliceEventCancelIndicator bool
 	OutOfNetworkIndicator      bool
 	ProgramSpliceFlag          bool
 	DurationFlag               bool
+	BreakAutoReturn            bool
+	BreakDuration              float64
 	SpliceImmediateFlag        bool
+	TimeSpecifiedFlag          bool
+	PTS                        float64
 	ComponentCount             uint64
 	Components                 []uint64
 	UniqueProgramId            uint64
 	AvailNum                   uint64
 	AvailExpected              uint64
+	Identifier                 uint64
 }
 
 // Decode the splice command values
@@ -79,8 +82,12 @@ func (cmd *SpCmd) Decode(bitn *gobit.Bitn, cmdtype uint64) {
 	if cmdtype == 6 {
 		cmd.TimeSignal(bitn)
 	}
-	// 7: Bandwidth_Reservation
-	// 255: Private_Command
+	if cmdtype == 7 {
+		cmd.BandwidthReservation(bitn)
+	}
+	if cmdtype == 255 {
+		cmd.PrivateCommand(bitn)
+	}
 }
 
 // ParseBreak parses out the ad break duration values
@@ -93,7 +100,7 @@ func (cmd *SpCmd) ParseBreak(bitn *gobit.Bitn) {
 // SpliceTime parses out the PTS value as needed
 func (cmd *SpCmd) SpliceTime(bitn *gobit.Bitn) {
 	cmd.TimeSpecifiedFlag = bitn.AsBool()
-	if cmd.TimeSpecifiedFlag == true {
+	if cmd.TimeSpecifiedFlag {
 		bitn.Forward(6)
 		cmd.PTS = bitn.As90k(33)
 	} else {
@@ -104,7 +111,7 @@ func (cmd *SpCmd) SpliceTime(bitn *gobit.Bitn) {
 // SpliceInsert handles SCTE 35 splice insert commands
 func (cmd *SpCmd) SpliceInsert(bitn *gobit.Bitn) {
 	cmd.Name = "Splice Insert"
-	cmd.SpliceEventId = bitn.AsInt(32)
+	cmd.SpliceEventId = bitn.AsHex(32)
 	cmd.SpliceEventCancelIndicator = bitn.AsBool()
 	bitn.Forward(7)
 	if !(cmd.SpliceEventCancelIndicator) {
@@ -114,24 +121,23 @@ func (cmd *SpCmd) SpliceInsert(bitn *gobit.Bitn) {
 		cmd.SpliceImmediateFlag = bitn.AsBool()
 		bitn.Forward(4)
 	}
-
-	if cmd.ProgramSpliceFlag == true {
-		if cmd.SpliceImmediateFlag == false {
+	if cmd.ProgramSpliceFlag {
+		if !(cmd.SpliceImmediateFlag) {
 			cmd.SpliceTime(bitn)
 		}
 	}
-	if cmd.ProgramSpliceFlag == false {
+	if !(cmd.ProgramSpliceFlag) {
 		cmd.ComponentCount = bitn.AsInt(8)
 		var Components [100]uint64
 		for i := range Components {
 			Components[i] = bitn.AsInt(8)
 		}
 		cmd.Components = Components[0:cmd.ComponentCount]
-		if cmd.SpliceImmediateFlag == false {
+		if !(cmd.SpliceImmediateFlag) {
 			cmd.SpliceTime(bitn)
 		}
 	}
-	if cmd.DurationFlag == true {
+	if cmd.DurationFlag {
 		cmd.ParseBreak(bitn)
 	}
 	cmd.UniqueProgramId = bitn.AsInt(16)
@@ -148,4 +154,15 @@ func (cmd *SpCmd) SpliceNull() {
 func (cmd *SpCmd) TimeSignal(bitn *gobit.Bitn) {
 	cmd.Name = "Time Signal"
 	cmd.SpliceTime(bitn)
+}
+
+// BandwidthReservation splice command
+func (cmd *SpCmd) BandwidthReservation(bitn *gobit.Bitn) {
+	cmd.Name = "Bandwidth Reservation"
+}
+
+// PrivateCommand splice command
+func (cmd *SpCmd) PrivateCommand(bitn *gobit.Bitn) {
+	cmd.Name = "Private Command"
+	cmd.Identifier = bitn.AsInt(32)
 }
